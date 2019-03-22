ad8a034eaedfb7ec2e9c71259304d9d278607f9e
lhban108 <626834490@qq.com>
2019-03-22 11:21:09 +0800

    文件类型判断和文件压缩

--- src/helper/route.js
+++ src/helper/route.js
@@ -8,14 +8,24 @@ const config = require('../config/defaultConfig')
 
 const tplPath = path.join(__dirname, '../template/dir.tpl') /* eslint-disable-line */
 const source = fs.readFileSync(tplPath)
-const template = Handlebars.compile(source.toString())
+const template = Handlebars.compile(source.toString()) // 模板
+
+// 压缩
+const compress = require('../helper/compress')
+    // 文件类型
+const mime = require('./mime')
 module.exports = async function(req, res, filePath) {
     try {
         const stats = await stat(filePath)
         if (stats.isFile()) {
             res.statusCode = 200
-            res.setHeader('Content-Type', 'text/palin')
-            fs.createReadStream(filePath).pipe(res)
+            const contentType = mime(filePath)
+            res.setHeader('Content-Type', contentType)
+            let rs = fs.createReadStream(filePath)
+            if (filePath.match(config.compress)) {
+                rs = compress(rs, req, res)
+            }
+            rs.pipe(res)
         } else if (stats.isDirectory()) {
             const files = await readdir(filePath)
             res.statusCode = 200;
